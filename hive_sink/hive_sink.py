6a03af28011d7826705dc646ba8a817953948f30
mvpboss1004 <mvpboss1004@126.com>
2021-06-23 16:00:31 +0800

    test insert hive ok

--- hive_sink/hive_sink.py
+++ hive_sink/hive_sink.py
@@ -1,6 +1,6 @@
 import sys
 from datetime import date
-from pyflink.table import EnvironmentSettings, BatchTableEnvironment
+from pyflink.table import EnvironmentSettings, BatchTableEnvironment, SqlDialect
 from pyflink.table import expressions as E, DataTypes as T
 from pyflink.table.catalog import HiveCatalog
 from pyflink.dataset import ExecutionEnvironment
@@ -13,18 +13,31 @@ if __name__ == '__main__':
         .build()
     bt_env = BatchTableEnvironment.create(environment_settings=b_set)
     b_env = ExecutionEnvironment.get_execution_environment()
+    bt_conf = bt_env.get_config()
+    
     bt_env.register_catalog('hive', HiveCatalog('hive', default_database=sys.argv[1], hive_conf_dir=sys.argv[2]))
     bt_env.use_catalog('hive')
+    bt_conf.set_sql_dialect(SqlDialect.HIVE)
     sql = f'''
         CREATE TABLE IF NOT EXISTS {sys.argv[3]} (
             _id INT,
             message STRING
         )
         PARTITIONED BY (partition_date DATE)
-        STORED AS orc
+        STORED AS ORC
     '''
-    today = str(date.today())
+    bt_env.execute_sql(sql)
     bt_env\
-        .from_elements([(1,'hello\nworld',today),(2,'are\nyou\nok',today)], ['_id','message','partition_date'])\
+        .from_elements(
+            elements = [
+                (1, 'hello\nworld', date.today()),
+                (2, 'are\nyou\nok', date.today())
+            ],
+            schema = T.ROW([
+                T.FIELD('_id', T.INT()),
+                T.FIELD('message', T.STRING()),
+                T.FIELD('partition_date', T.DATE())
+            ])
+        )\
         .execute_insert(sys.argv[3], overwrite=True)
 
