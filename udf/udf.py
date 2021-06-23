4d911e5b33407813d84374eb5ffabeaeb914dcac
mvpboss1004 <mvpboss1004@126.com>
2021-06-23 13:24:06 +0800

    udf test ok

--- udf/udf.py
+++ udf/udf.py
@@ -1,20 +1,20 @@
 from pyflink.table import EnvironmentSettings, BatchTableEnvironment
 from pyflink.table import expressions as E, DataTypes as T
-from pyflinm.table.udf import udf, udaf
+from pyflink.table.udf import udf, udaf
 from pyflink.dataset import ExecutionEnvironment
 from functools import reduce
 from operator import or_
 
 @udf(result_type=T.INT())
-def inet_aton(ip:str) -> int:
+def inet_aton(ip):
     try:
-        sum([int(n)<<(8*(3-i)) for n,i in zip(ip.split('.'),range(4))])
+        return int(sum([int(n)<<(8*(3-i)) for n,i in zip(ip.split('.'), range(4))]))
     except Exception as e:
         return None
 
-@udaf(result_type=T.INT())
-def bit_or_aggr(flags: int) -> int:
-    return reduce(flags, or_, 0)
+@udaf(result_type=T.INT(), accumulator_type=T.ARRAY(T.INT()), func_type='pandas')
+def bit_or_aggr(flags):
+    return reduce(or_, flags, 0)
 
 if __name__ == '__main__':
     b_set = EnvironmentSettings\
@@ -24,12 +24,12 @@ if __name__ == '__main__':
         .build()
     bt_env = BatchTableEnvironment.create(environment_settings=b_set)
     b_env = ExecutionEnvironment.get_execution_environment()
-    bt_env.register_function('INET_ATON', inet_aton)
-    bt_env.register_function('BIT_OR_AGGR', inet_aton)
+    bt_env.create_temporary_system_function('INET_ATON', inet_aton)
+    bt_env.create_temporary_system_function('BIT_OR_AGGR', bit_or_aggr)
     bt_env\
         .from_elements([('0.0.0.1',1),('0.0.0.1',2)], ['ip','flag'])\
-        .group_by(E.call_sql('INET_ATON(ip) AS ip_int'))\
-        .aggregate(bit_or_aggr(E.col('flag')).alias('flag_agg'))\
+        .group_by('ip')\
+        .select(inet_aton(E.col('ip')), bit_or_aggr(E.col('flag')))\
         .execute()\
         .print()
-    
+
