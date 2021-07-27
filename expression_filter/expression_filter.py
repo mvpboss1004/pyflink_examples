95c970c9443bf105eb818d03dc112d1b4f0240e3
mvpboss1004 <mvpboss1004@126.com>
2021-07-27 18:05:03 +0800

    add readme

--- expression_filter/expression_filter.py
+++ expression_filter/expression_filter.py
@@ -1,6 +1,5 @@
 from pyflink.table import EnvironmentSettings, BatchTableEnvironment
 from pyflink.table import expressions as E
-from pyflink.table.module import HiveModule
 from pyflink.dataset import ExecutionEnvironment
 
 if __name__ == '__main__':
@@ -11,11 +10,15 @@ if __name__ == '__main__':
         .build()
     bt_env = BatchTableEnvironment.create(environment_settings=b_set)
     b_env = ExecutionEnvironment.get_execution_environment()
-    bt_env.load_module('hive', HiveModule())
-    print('Available functions: ', bt_env.list_functions())
     t0 = bt_env.from_elements([('Alice',1),('Bob',2)], ['name','age'])
+
+    condition = "age IN (2,3)"
+    # These codes won't work.
+    # t0.filter(condition).execute().print()
+
+    # These codes work.
     bt_env.register_table('t0', t0)
     bt_env\
-        .sql_query('SELECT COLLECT_SET(name) FROM t0')\
+        .sql_query(f'SELECT * FROM t0 WHERE {condition}')\
         .execute()\
         .print()
