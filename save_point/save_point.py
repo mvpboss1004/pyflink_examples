c17be5b633c40d4a46bbf22ba889af1b651eb2f5
mvpboss1004 <mvpboss1004@126.com>
2022-02-11 18:57:17 +0800

    migrate to version 1.14

--- save_point/save_point.py
+++ save_point/save_point.py
@@ -1,31 +1,38 @@
 import sys
-import json
-from pyflink.table import EnvironmentSettings, StreamTableEnvironment, expressions as E
-from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import EnvironmentSettings, TableEnvironment, expressions as E
 
 if __name__ == '__main__':
-    s_set = EnvironmentSettings\
-        .new_instance()\
-        .in_streaming_mode()\
-        .use_blink_planner()\
-        .build()
+    s_set = EnvironmentSettings.in_streaming_mode()
     st_env = StreamTableEnvironment.create(environment_settings=s_set)
-    s_env = StreamExecutionEnvironment.get_execution_environment() 
 
     st_env.execute_sql(f'''
-        CREATE TABLE json_input (
+        CREATE TEMPORARY TABLE kafka_in (
             `id` INT
         ) WITH (
             'connector' = 'kafka',
             'properties.bootstrap.servers' = '{sys.argv[1]}',
+            'properties.group.id' = 'save_point',
             'topic' = '{sys.argv[2]}',
             'format' = 'json',
             'json.fail-on-missing-field' = 'false',
             'json.ignore-parse-errors' = 'true'
         )'''
     )
+    st_env.execute_sql(f'''
+        CREATE TEMPORARY TABLE kafka_out (
+            `id` INT,
+            `name` STRING
+        ) WITH (
+            'connector' = 'kafka',
+            'properties.bootstrap.servers' = '{sys.argv[1]}',
+            'topic' = '{sys.argv[3]}',
+            'scan.startup.mode' = 'earliest-offset',
+            'format' = 'json'
+        )'''
+    )
     st_env\
-        .from_path('json_input')\
-        .add_columns(E.lit(sys.argv[3]).alias('name'))\
-        .execute()\
-        .print()
+        .from_path('kafka_in')\
+        .add_columns(E.lit(sys.argv[4]).alias('name'))\
+        .insert_into('kafka_out')
+    st_env.execute('save_point')
+
