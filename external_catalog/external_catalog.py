3bebf2ccb8ecb258354efc703b76b5cf7f430d7c
mvpboss1004 <mvpboss1004@126.com>
2022-02-11 15:58:17 +0800

    migrate to version 1.14

--- external_catalog/external_catalog.py
+++ external_catalog/external_catalog.py
@@ -1,17 +1,10 @@
 import sys
-from pyflink.table import EnvironmentSettings, BatchTableEnvironment
-from pyflink.table import expressions as E
+from pyflink.table import EnvironmentSettings, TableEnvironment
 from pyflink.table.catalog import HiveCatalog, JdbcCatalog
-from pyflink.dataset import ExecutionEnvironment
 
 if __name__ == '__main__':
-    b_set = EnvironmentSettings\
-        .new_instance()\
-        .in_batch_mode()\
-        .use_blink_planner()\
-        .build()
-    bt_env = BatchTableEnvironment.create(environment_settings=b_set)
-    b_env = ExecutionEnvironment.get_execution_environment()
+    b_set = EnvironmentSettings.in_batch_mode()
+    bt_env = TableEnvironment.create(environment_settings=b_set)
 
     for name, catalog in [
         ('jdbc', JdbcCatalog('jdbc', base_url=sys.argv[1], default_database=sys.argv[2], username=sys.argv[3], pwd=sys.argv[4])),
