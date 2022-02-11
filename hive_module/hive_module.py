10aa5c14608e94dd1a4dfc856af2b1b05ca18ed1
mvpboss1004 <mvpboss1004@126.com>
2022-02-11 17:23:05 +0800

    migrate to version 1.14

--- hive_module/hive_module.py
+++ hive_module/hive_module.py
@@ -1,16 +1,10 @@
-from pyflink.table import EnvironmentSettings, BatchTableEnvironment
-from pyflink.table import expressions as E
+from pyflink.table import EnvironmentSettings, TableEnvironment
 from pyflink.table.module import HiveModule
-from pyflink.dataset import ExecutionEnvironment
 
 if __name__ == '__main__':
-    b_set = EnvironmentSettings\
-        .new_instance()\
-        .in_batch_mode()\
-        .use_blink_planner()\
-        .build()
-    bt_env = BatchTableEnvironment.create(environment_settings=b_set)
-    b_env = ExecutionEnvironment.get_execution_environment()
+    b_set = EnvironmentSettings.in_batch_mode()
+    bt_env = TableEnvironment.create(environment_settings=b_set)
+    
     bt_env.load_module('hive', HiveModule())
     print('Available functions: ', bt_env.list_functions())
     t0 = bt_env.from_elements([('Alice',1),('Bob',2)], ['name','age'])
